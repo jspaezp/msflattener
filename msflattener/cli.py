d1490a14c9bea503dddbe90f453d6ccae713d89e
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2023-05-05 12:24:16 -0700

    added cli boilerplate

--- msflattener/cli.py
+++ msflattener/cli.py
@@ -5,16 +5,25 @@ from rich_click import click as click
 # msflattener --no_collapse_ims/collapse_ims
 # msflattener --min_peaks 10
 
-@click.command()
-@click.option('--min_peaks', default=10, help='Minimum number of peaks to keep a spectrum')
-@click.option('--file', type=click.Path(exists=True), help="File to use as input")
-def main(min_peaks, file):
+
+@click.group()
+def cli():
     pass
 
-@click.command()
-@click.option('--min_peaks', default=10, help='Number of greetings.')
+@cli.command()
+@click.option('--file', type=click.Path(exists=True), help="File to use as input")
+@click.option('--output', type=click.Path(exists=False), help="Name of the output file")
+@click.option('--to_mzml', 'out_format', flag_value='upper',
+              default=True)
+@click.option('--to_parquet', 'out_format', flag_value='lower')
+@click.option('--min_peaks', default=10, help='Minimum number of peaks to keep a spectrum')
+def bruker(file, out_format, min_peaks):
+    pass
+
+@cli.command()
 @click.option('--file', prompt='Your name',
               help='The person to greet.')
-def convert_to_mzml():
+@click.option('--min_peaks', default=10, help='Number of greetings.')
+def mzml():
     pass
 

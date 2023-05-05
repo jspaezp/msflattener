300cb330fe56cabb3ee5972f8f5cc34cc95c3cd0
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2023-05-05 12:04:58 -0700

    added cli boilerplate

--- msflattener/cli.py
+++ msflattener/cli.py
@@ -4,3 +4,17 @@ from rich_click import click as click
 # msflattener --to_mzml
 # msflattener --no_collapse_ims/collapse_ims
 # msflattener --min_peaks 10
+
+@click.command()
+@click.option('--min_peaks', default=10, help='Minimum number of peaks to keep a spectrum')
+@click.option('--file', type=click.Path(exists=True), help="File to use as input")
+def main(min_peaks, file):
+    pass
+
+@click.command()
+@click.option('--min_peaks', default=10, help='Number of greetings.')
+@click.option('--file', prompt='Your name',
+              help='The person to greet.')
+def convert_to_mzml():
+    pass
+

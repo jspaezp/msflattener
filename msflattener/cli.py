d9fbf17d584362f751577e5d000d6fd5cc0396e1
J. Sebastian Paez <jspaezp@gmail.com>
2023-07-24 10:06:01 -0700

    updated cli and readme

--- msflattener/cli.py
+++ msflattener/cli.py
@@ -1,7 +1,9 @@
 import rich_click as click
+from loguru import logger
 
 from .bruker import get_timstof_data
 from .mzml import get_mzml_data, write_mzml
+from .encyclopedia import write_encyclopedia
 
 
 @click.group()
@@ -21,6 +23,7 @@ def cli():
 )
 @click.option("--to_mzml", "out_format", flag_value="mzml", default=False)
 @click.option("--to_parquet", "out_format", flag_value="parquet", default=True)
+@click.option("--to_dia", "out_format", flag_value="dia", default=True)
 @click.option(
     "--min_peaks", default=5, help="Minimum number of peaks to keep a spectrum"
 )
@@ -40,9 +43,14 @@ def bruker(file, output, out_format, min_peaks, progbar, centroid):
         file, min_peaks=min_peaks, progbar=progbar, centroid=centroid
     )
     if out_format == "parquet":
+        logger.info("Writing parquet")
         dat.write_parquet(output)
     elif out_format == "mzml":
+        logger.info("Writing mzML")
         write_mzml(dat, output)
+    elif out_format == "dia":
+        logger.info("Writing encyclopedia dia file")
+        write_encyclopedia(dat, output)
     else:
         raise RuntimeError
 
